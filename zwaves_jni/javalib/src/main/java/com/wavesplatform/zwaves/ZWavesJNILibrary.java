9a4ead88b3de359770249d6b2cc58bd07322c990
Vyatcheslav Suharnikov <arz.freezy@gmail.com>
2023-10-16 14:22:11 +0400

    Fix a native library location for osx/amd64

--- zwaves_jni/javalib/src/main/java/com/wavesplatform/zwaves/ZWavesJNILibrary.java
+++ zwaves_jni/javalib/src/main/java/com/wavesplatform/zwaves/ZWavesJNILibrary.java
@@ -2,6 +2,7 @@ package com.wavesplatform.zwaves;
 
 public class ZWavesJNILibrary {
     static {
+        // Our layout: "META-INF/native/${platform}/${arch}/${library[-version]}"
         new JNILibrary("zwaves_jni", ZWavesJNILibrary.class).load();
     }
 
